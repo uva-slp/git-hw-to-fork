77665734bdef688ecd98a425b702f53d58111936
seamuslowry <spl6yc@virginia.edu>
2015-08-30 18:14:10 -0400

    Adding program

---
+++ Lowry.java
@@ -0,0 +1,14 @@
+/*
+ * I wrote this in high school
+ * It annoyed our teacher
+ * Apparently, it doesn't work on some systems/interfaces
+ * If it does, it is moderately amusing
+ */
+
+public class Lowry{
+	public static void main(String[] args){
+		while(true){
+			System.out.println((char)7);
+		}
+	}
+}
